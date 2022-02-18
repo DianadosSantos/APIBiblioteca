aa46b37c3f593d19d082c045f58b275d9dd86231
koblinski13 <vinicius.vkmoraes@gmail.com>
2022-02-18 18:00:48 -0300

    conclusão métodos usuários

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('apibiblioteca' , 'root', '123456', {
+const sequelize = new Sequelize('apibiblioteca' , 'root', 'pizzadecalabresa13', {
     host:'localhost',
     dialect: 'mysql'
 });
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const UsuarioController = require('../controllers/UsuarioController')
 
-router.post('/add', UsuarioController.createUsuario)
-router.get('/',UsuarioController.mostrarUsuario)
+router.post('/add', UsuarioController.criarUsuario)
+router.get('/',UsuarioController.listarUsuario)
+router.get('/:id', UsuarioController.mostrarUsuario)
+router.post('/atualizar', UsuarioController.atualizarUsuario)
 
 
 module.exports = router
