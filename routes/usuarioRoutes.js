094ea834713339cb2eca110b0214dcc289a58cf4
koblinski13 <vinicius.vkmoraes@gmail.com>
2022-02-20 17:32:29 -0300

    implementação método deletar

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -7,6 +7,7 @@ router.post('/add', UsuarioController.criarUsuario)
 router.get('/',UsuarioController.listarUsuario)
 router.get('/:id', UsuarioController.mostrarUsuario)
 router.post('/atualizar', UsuarioController.atualizarUsuario)
+router.post('/remover', UsuarioController.removerUsuario)
 
 
 module.exports = router
