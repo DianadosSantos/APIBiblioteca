e5d83be0a9caa43581a360bc1cd9dab636e432e8
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-02-20 18:18:34 -0300

    Começo do CRUD de Autores.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const conn = require('./db/conn');
-const porta = process.env.PORT || 3000
+const conn = require("./db/conn");
+const porta = process.env.PORT || 3000;
 
-const Usuario = require('./models/Usuarios')
-const usuarioRoutes = require('./routes/usuarioRoutes')
+const Usuario = require("./models/Usuarios");
+const usuarioRoutes = require("./routes/usuarioRoutes");
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+const Autor = require("./models/Autores");
+const autorRoutes = require("./routes/autoresRoutes");
 
-app.use('/usuarios', usuarioRoutes)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/usuarios", usuarioRoutes);
+app.use("/autores", autorRoutes);
 
 conn
-.sync()
-//.sync({force: true})
-.then(() =>{
-    app.listen(porta)
-})
-.catch((err) => console.log(err))
+  .sync()
+  //.sync({force: true})
+  .then(() => {
+    app.listen(porta);
+  })
+  .catch((err) => console.log(err));
